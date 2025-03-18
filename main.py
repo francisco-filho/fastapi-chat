9feec9c3788fb8c1bd84bfceac248fb698d04464
F3445038 Francisco Manoel Da Silva Filho <francisco.filho@bb.com.br>
2025-03-18 19:46:41 -0300

    Proxy conenctions to gpt

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
+import os
 from fastapi import FastAPI
+from openai import OpenAI
+from dotenv import load_dotenv
+from pydantic import BaseModel
+
+load_dotenv()
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "hello world"}
+MODEL="gpt-4o-mini"
 
-@app.get("/item/{item_id}")
-def get_item(item_id: int):
-    return {"item_id": item_id}
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+class ChatMessage(BaseModel):
+    role: str
+    content: str
+
+
+@app.post("/chat")
+async def chat(msg: str):
+    message = ChatMessage(role="user", content=msg)
+    completion = client.chat.completions.create(
+        model=MODEL,
+        messages=[message]
+    )
+    return completion.choices[0].message.content
+
