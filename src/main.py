43e92666e6c4443557c8335a5a52117d87bde91e
F3445038 Francisco Manoel Da Silva Filho <francisco.filho@bb.com.br>
2025-03-25 07:35:53 -0300

    Add multi-model support with dynamic configuration
    fixes AB#249

--- src/main.py
+++ src/main.py
@@ -22,20 +22,40 @@ app.add_middleware(
     allow_methods=['*']
 )
 
-MODEL="gpt-4o-mini"
+#MODEL="gpt-4o-mini"
+
+config = {
+    'gpt-4o-mini': {
+        'url': 'https://api.openai.com/v1',
+        'api_key': os.environ.get("OPENAI_API_KEY"),
+    },
+    'deepseek-r1:7b': {
+        'url': 'https://127.0.0.1:11434/v1',
+        'api_key': 'ollama',
+    },
+}
+
+
+MODEL = 'gpt-4o-mini'
 
 
 client = OpenAI(
-    api_key=os.environ.get("OPENAI_API_KEY"),
+    base_url=config[MODEL]['url'],
+    api_key=config[MODEL]['api_key']
 )
 
 class Chat(BaseModel):
     id: int
     created_at: datetime 
 
-class ChatMessage(BaseModel):
+class Message(BaseModel):
     role: str
     content: str
+
+class ChatMessage(BaseModel):
+    model: str = 'gpt-4o-mini'
+    messages: list[Message] = []
+
 
 app.history = []
 
@@ -44,24 +64,35 @@ def load_chat(chat_id: int):
     with psycopg.connect(f"host=localhost dbname=localchat user={DB_USER} password={DB_PASSWORD}") as conn:
         with conn.cursor() as cur:
             cur.execute("select c.role, c.content from chat_messages c where chat_id = %s", (chat_id, ))
-            app.history = ([ChatMessage(role=c[0], content=c[1]) for c in cur.fetchall()])
+            app.history = ([Message(role=c[0], content=c[1]) for c in cur.fetchall()])
             print('load_chats', app.history)
             return app.history
      
 
 @app.post("/chat/{chat_id}")
-async def chat(chat_id: int, message: ChatMessage):
+def chat(chat_id: int, chat_message: ChatMessage):
+    global MODEL
+    global client
+    if chat_message.model != MODEL:
+        MODEL = chat_message.model
+        client = OpenAI(
+            base_url=config[MODEL]['url'],
+            api_key=config[MODEL]['api_key']
+        )
+
+
+    message = chat_message.messages[0]
     app.history.append(message)
-    persist_chat(chat_id, message.role, message.content)
+    persist_chat(chat_id, message.role, message.content, model=chat_message.model)
     completion = client.chat.completions.create(
-        model=MODEL,
+        model=chat_message.model,
         messages=app.history
     )
     response = completion.choices[0].message
-    app.history.append(ChatMessage(role=response.role, content=response.content))
-    print(app.history)
-    persist_chat(chat_id, 'assistant', response.content)
-    return ChatMessage(role="assistant", content=response.content)
+    app.history.append(Message(role=response.role, content=response.content))
+    #print(app.history)
+    persist_chat(chat_id, 'assistant', response.content, model=chat_message.model)
+    return Message(role="assistant", content=response.content)
 
 @app.post("/chat", status_code=201)
 def create_chat():
